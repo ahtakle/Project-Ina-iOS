e66e58de28e65b00ee24f93c71f637a3923fe824
zskemp <zrskemp@gmail.com>
2017-06-01 14:12:36 -0400

    Fixed calendar

--- Ina/Ina/CalendarController.swift
+++ Ina/Ina/CalendarController.swift
@@ -9,29 +9,18 @@
 import Foundation
 import UIKit
 
-class CalendarController: UIViewController {
+class CalendarController: UIViewController, UIWebViewDelegate {
     
-    @IBOutlet weak var webView: UIWebView!
-    
+    @IBOutlet var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView.delegate = self
         
-//        let url = URL(string: "https://www.googleapis.com/calendar/v3/calendars/zrskemp.gmail.com/events?maxResults=15&key=AIzaSyBmI5kwmgjJWgNIIpYdxNoR3a0gKU5tL2A")
-//        
-//        webView.loadRequest(URLRequest(url: url!))
-//        var HTML = "<iframe src='https://calendar.google.com/calendar/embed?src=" + "svttqkqr143jcadd858a3da8u8%40group.calendar.google.com&ctz=America/New_York' style='border: 0'" + "width='" + 150 + "' height='" + 200 + "' frameborder='0' scrolling='no'></iframe>"
-//        webView.loadHTMLString(HTML, baseURL: <#T##URL?#>)
+        let HTML = "<iframe src='https://calendar.google.com/calendar/embed?src=svttqkqr143jcadd858a3da8u8%40group.calendar.google.com&ctz=America/New_York' style='border: 0' width='375' height='650' frameborder='0' scrolling='no'></iframe>"
+        webView.loadHTMLString(HTML, baseURL: nil)
 
-    }
-    
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        webView.frame.size.height = 1
-        let height = webView.scrollView.contentSize.height
-        var wvRect = webView.frame
-        wvRect.size.height = height
-        webView.frame = wvRect
     }
     
     override func didReceiveMemoryWarning() {
