c0bdb3b1c5501e3f8f82592cfe497361db1da7d8
schoi1826 <schoi1826@gmail.com>
2019-05-24 13:08:28 -0700

    Add map data

--- Ina/Ina/Location.swift
+++ Ina/Ina/Location.swift
@@ -27,10 +27,14 @@ class Location: NSObject, MKAnnotation{
 class LocationList: NSObject {
     var resource = [Location]()
     override init(){
-        resource += [Location(name:"Head Start", address: "123 Sisseton",lat:45.5681150,long:-97.0669610  )]
-        resource += [Location(name:"Tribal Admin Building", address: "123 Sisseton",lat:45.5677590,long: -97.0711610 )]
-        resource += [Location(name:"test1", address: "123 Sisseton",lat:45.6568280,long: -97.0160580 )]
-        resource += [Location(name:"SWO Health Nurse Clinic", address: "123 Sisseton",lat:45.6674190,long:-97.0457440  )]
-        resource += [Location(name:"Dakota Pride Center", address: "123 Sisseton",lat:45.5636240,long:-97.0763670  )]
+        resource += [Location(name:"Head Start", address: "100 Veterans Memorial Dr",lat:45.5681150,long:-97.0669610  )]
+        resource += [Location(name:"SWO Early Childhood Intervention", address: "Goodwill Township",lat:45.5677590,long: -97.0711610 )]
+        resource += [Location(name:"IHS", address: "100 Lake Traverse Dr",lat:45.6568280,long: -97.0160580 )]
+        resource += [Location(name:"Roberts County", address: "405 Chestnut St E",lat:45.6674190,long:-97.0457440  )]
+        resource += [Location(name:"SWO Pride", address: "Dakota Ave",lat:45.5636240,long:-97.0763670  )]
+        resource += [Location(name:"Coteau", address: "205 Orchard Dr",lat:45.657723,long: -97.050173 )]
+        resource += [Location(name:"GPTCHB", address: "1770 Rand Rd",lat:44.101915,long: -103.263103 )]
+        resource += [Location(name:"Little Steps Daycare", address: "100 Veterans Memorial Dr",lat:45.567646,long: -97.069341 )]
+        resource += [Location(name:"WIC", address: "99-1 119th St",lat:45.660120,long: -97.050772 )]
     }
 }
